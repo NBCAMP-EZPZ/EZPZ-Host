95d4caece2a6d432ee2af288bdb7f7d3dbfe31cf
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-23 20:04:08 +0900

    Feat [#8] : 개최자 엔티티 생성

---
+++ src/main/java/com/sparta/ezpzhost/domain/host/entity/Host.java
@@ -0,0 +1,42 @@
+package com.sparta.ezpzhost.domain.host.entity;
+
+import com.sparta.ezpzhost.common.entity.Timestamped;
+import com.sparta.ezpzhost.domain.host.dto.SignupRequestDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Host extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "host_id")
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String companyName;
+
+    private String businessNumber;
+
+    private Host(SignupRequestDto dto, String encodedPassword) {
+        this.username = dto.getUsername();
+        this.password = encodedPassword;
+        this.email = dto.getEmail();
+        this.companyName = dto.getCompanyName();
+        this.businessNumber = dto.getBusinessNumber();
+    }
+
+    public static Host of(SignupRequestDto dto, String encodedPassword) {
+        return new Host(dto, encodedPassword);
+    }
+
+}
