a5cccbf0642722a71eb5831f7f373173d6dd8266
llocr <36-96@naver.com>
2024-07-24 11:47:32 +0900

    Refactor [#2] : 어노테이션 수정

--- src/main/java/com/sparta/ezpzhost/EzpzHostApplication.java
+++ src/main/java/com/sparta/ezpzhost/EzpzHostApplication.java
@@ -2,9 +2,8 @@ package com.sparta.ezpzhost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class EzpzHostApplication {
 
 	public static void main(String[] args) {
