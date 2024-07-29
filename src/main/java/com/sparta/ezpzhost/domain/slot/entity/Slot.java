20c5d940d42985eff4814be9dd46948313b05de5
llocr <36-96@naver.com>
2024-07-29 16:19:37 +0900

    Feat [#27] : Index 적용

--- src/main/java/com/sparta/ezpzhost/domain/slot/entity/Slot.java
+++ src/main/java/com/sparta/ezpzhost/domain/slot/entity/Slot.java
@@ -20,9 +20,11 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +32,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "slot", indexes = {
+	@Index(name = "idx_popup_slot", columnList = "popup_id, slot_id")
+})
 public class Slot extends Timestamped {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
