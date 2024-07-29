0c8c5dc6132e1ada94da68af6285792caba29592
M1ngD0ng <alswjd6807@naver.com>
2024-07-29 16:13:55 +0900

    Fix [#25] : 주문 조회 요청 버그 해결

--- src/main/java/com/sparta/ezpzhost/domain/order/dto/OrderRequestDto.java
+++ src/main/java/com/sparta/ezpzhost/domain/order/dto/OrderRequestDto.java
@@ -10,4 +10,10 @@ public class OrderRequestDto {
     private String searchType = "all"; // 기본값 설정
     private Long itemId = -1L;         // 기본값 설정
     private String orderStatus = "all"; // 기본값 설정
+
+    public OrderRequestDto() {
+        this.searchType = "all";
+        this.itemId = -1L;
+        this.orderStatus = "all";
+    }
 }
