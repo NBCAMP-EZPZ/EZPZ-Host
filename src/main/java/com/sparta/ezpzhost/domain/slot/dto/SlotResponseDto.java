fa479bddc1f13ec3088616bf533d627869e98215
M1ngD0ng <alswjd6807@naver.com>
2024-08-01 20:52:02 +0900

    Refactor [#30] : SlotResponseDto Id 추가

--- src/main/java/com/sparta/ezpzhost/domain/slot/dto/SlotResponseDto.java
+++ src/main/java/com/sparta/ezpzhost/domain/slot/dto/SlotResponseDto.java
@@ -1,34 +1,35 @@
 package com.sparta.ezpzhost.domain.slot.dto;
 
-import java.util.List;
-
 import com.sparta.ezpzhost.domain.slot.entity.Slot;
-
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class SlotResponseDto {
-	private String slotDate;
-	private String slotTime;
-	private int availableCount;
-	private int totalCount;
-	private String slotStatus;
-	
-	private SlotResponseDto(Slot slot) {
-		this.slotDate = slot.getSlotDate().toString();
-		this.slotTime = slot.getSlotTime().toString();
-		this.availableCount = slot.getAvailableCount();
-		this.totalCount = slot.getTotalCount();
-		this.slotStatus = slot.getSlotStatus().toString();
-	}
-	
-	public static SlotResponseDto of(Slot slot) {
-		return new SlotResponseDto(slot);
-	}
-	
-	public static List<SlotResponseDto> listOf(List<Slot> slotList) {
-		return slotList.stream()
-			.map(SlotResponseDto::of)
-			.toList();
-	}
+
+    private Long id;
+    private String slotDate;
+    private String slotTime;
+    private int availableCount;
+    private int totalCount;
+    private String slotStatus;
+
+    private SlotResponseDto(Slot slot) {
+        this.id = slot.getId();
+        this.slotDate = slot.getSlotDate().toString();
+        this.slotTime = slot.getSlotTime().toString();
+        this.availableCount = slot.getAvailableCount();
+        this.totalCount = slot.getTotalCount();
+        this.slotStatus = slot.getSlotStatus().toString();
+    }
+
+    public static SlotResponseDto of(Slot slot) {
+        return new SlotResponseDto(slot);
+    }
+
+    public static List<SlotResponseDto> listOf(List<Slot> slotList) {
+        return slotList.stream()
+                .map(SlotResponseDto::of)
+                .toList();
+    }
 }
