2de2107a65091e640ed2c53e0ade6bd25c6d3427
M1ngD0ng <alswjd6807@naver.com>
2024-08-01 20:51:39 +0900

    Refactor [#30] : review Entity 추가 & Reservation에 review list 추가
    - slot hard delete 시 문제 발생하여 수정

--- src/main/java/com/sparta/ezpzhost/domain/reservation/entity/Reservation.java
+++ src/main/java/com/sparta/ezpzhost/domain/reservation/entity/Reservation.java
@@ -1,9 +1,10 @@
 package com.sparta.ezpzhost.domain.reservation.entity;
 
 import com.sparta.ezpzhost.domain.reservation.enums.ReservationStatus;
+import com.sparta.ezpzhost.domain.review.entity.Review;
 import com.sparta.ezpzhost.domain.slot.entity.Slot;
 import com.sparta.ezpzhost.domain.user.entity.User;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,28 +15,34 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.Getter;
 
 @Entity
 @Getter
 public class Reservation {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "reservation_id")
-	private Long id;
-	
-	@Column(nullable = false)
-	private int numberOfPersons;
-	
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private ReservationStatus reservationStatus;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "slot_id", nullable = false)
-	private Slot slot;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private int numberOfPersons;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus reservationStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "slot_id", nullable = false)
+    private Slot slot;
+
+    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
 }
