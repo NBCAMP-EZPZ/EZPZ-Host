265bebbe97b1afa60e77b9b8dea4b00c819fa525
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-02 12:06:34 +0900

    Feat [#32] : Add DistributedLockAspect

---
+++ src/main/java/com/sparta/ezpzhost/common/lock/TransactionForAop.java
@@ -0,0 +1,19 @@
+package com.sparta.ezpzhost.common.lock;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * AOP 에서 트랜잭션 분리를 위한 클래스
+ */
+@Component
+public class TransactionForAop {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+
+}
--- src/test/java/com/sparta/ezpzhost/EzpzHostApplicationTest.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.ezpzhost;
-
-class EzpzHostApplicationTest {
-
-}
