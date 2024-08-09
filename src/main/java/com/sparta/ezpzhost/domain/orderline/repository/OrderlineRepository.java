35463a729abdf6a77b1f6b68cbadd39ad6bc1312
M1ngD0ng <alswjd6807@naver.com>
2024-08-10 00:19:32 +0900

    Refactor [#36] : 사용하지 않는 코드 삭제

--- src/main/java/com/sparta/ezpzhost/domain/orderline/repository/OrderlineRepository.java
+++ src/main/java/com/sparta/ezpzhost/domain/orderline/repository/OrderlineRepository.java
@@ -3,7 +3,6 @@ package com.sparta.ezpzhost.domain.orderline.repository;
 import com.sparta.ezpzhost.domain.orderline.entity.Orderline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderlineRepository extends JpaRepository<Orderline, Long>,
-        OrderlineRepositoryCustom {
+public interface OrderlineRepository extends JpaRepository<Orderline, Long> {
 
 }
